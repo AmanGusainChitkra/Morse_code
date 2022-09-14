fa459ba3a97355383a144d0a1997059a7f9dc5fa
AmanGusainChitkra <113379582+AmanGusainChitkra@users.noreply.github.com>
2022-09-14 15:06:10 +0530

    Update basic_code.cpp

--- basic_code.cpp
+++ basic_code.cpp
@@ -1,7 +1,7 @@
 
 // the setup function runs once when you press reset or power the board
-const int same_delay = 1000;
-int letter_delay = 3000;  
+const int same_delay = 1000;    //one unit delay
+int letter_delay = 3000;    //three unit delay
 int next_line = 5000; 
 //1 = dot, 2 = dash, 3 = empty;
 void setup() {
@@ -36,18 +36,18 @@ void loop() {
 }
 
 
-void dot()
+void dot()    //blinks dot.
 {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(same_delay);                       // wait for a second
+  delay(same_delay);                       //wait for 1unit time.
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 }
 
 
-void dash()
+void dash()   //blinks dash
 {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(letter_delay);                       // wait for a second
+  delay(letter_delay);                      //wait for 3unit time.
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
  }
 
